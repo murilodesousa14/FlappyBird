ac11bc96d32442501e12445774447f796d69fe69
Murilo <murilodesousa454@gmail.com>
2026-08-11 19:37:46 -0300

    Create ScoreManager.hpp

---
+++ include/ScoreManager.hpp
@@ -0,0 +1,44 @@
+#ifndef SCORE_MANAGER_HPP
+#define SCORE_MANAGER_HPP
+
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class ScoreManager {
+private:
+    string filename;
+    int highScore;
+
+public:
+    ScoreManager(const string& file = "save/highscore.txt") : filename(file), highScore(0) {
+        loadHighScore();
+    }
+
+    int loadHighScore() {
+        ifstream inFile(filename);
+        if (inFile.is_open()) {
+            inFile >> highScore;
+            inFile.close();
+        } else {
+            highScore = 0;
+        }
+        return highScore;
+    }
+
+    void saveHighScore(int score) {
+        if (score > highScore) {
+            highScore = score;
+            ofstream outFile(filename);
+            if (outFile.is_open()) {
+                outFile << highScore;
+                outFile.close();
+            }
+        }
+    }
+
+    int getHighScore() const { return highScore; }
+};
+
+#endif
