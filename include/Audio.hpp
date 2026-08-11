b33a4b8981b25554997af5c7e64094d4e1bc6d45
Murilo <murilodesousa454@gmail.com>
2026-08-11 19:37:52 -0300

    Create Audio.hpp

---
+++ include/Audio.hpp
@@ -0,0 +1,33 @@
+#ifndef AUDIO_HPP
+#define AUDIO_HPP
+
+#include <cstdlib>
+#include <thread>
+
+using namespace std;
+
+inline void playPointSound() {
+    thread([]() {
+        int res = system("paplay assets/sounds/point.wav > /dev/null 2>&1");
+        if (res != 0) {
+            res = system("canberra-gtk-play -f assets/sounds/point.wav > /dev/null 2>&1");
+        }
+        if (res != 0) {
+            system("aplay assets/sounds/point.wav > /dev/null 2>&1");
+        }
+    }).detach();
+}
+
+inline void playDieSound() {
+    thread([]() {
+        int res = system("paplay assets/sounds/die.wav > /dev/null 2>&1");
+        if (res != 0) {
+            res = system("canberra-gtk-play -f assets/sounds/die.wav > /dev/null 2>&1");
+        }
+        if (res != 0) {
+            system("aplay assets/sounds/die.wav > /dev/null 2>&1");
+        }
+    }).detach();
+}
+
+#endif
