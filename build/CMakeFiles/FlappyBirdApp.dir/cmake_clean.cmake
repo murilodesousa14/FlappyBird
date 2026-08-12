2b62a98434a0bf52f0d5c7cfefc0143389c1efda
Murilo <murilodesousa454@gmail.com>
2026-08-11 22:22:41 -0300

    Create cmake_clean.cmake

---
+++ build/CMakeFiles/FlappyBirdApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FlappyBirdApp.dir/src/main.cpp.o"
+  "CMakeFiles/FlappyBirdApp.dir/src/main.cpp.o.d"
+  "FlappyBirdApp"
+  "FlappyBirdApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FlappyBirdApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
